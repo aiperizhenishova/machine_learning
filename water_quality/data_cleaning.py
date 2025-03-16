e84a4db12852f6897369c3168ebe637559592725
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2025-03-17 00:10:23 +0600

    data cleanig

--- water_quality/data_cleaning.py
+++ water_quality/data_cleaning.py
@@ -1,27 +1,34 @@
 import pandas as pd
 
-# Load the CSV file
-df = pd.read_csv('water_quality/cafe_sales.csv')
 
-# Clean the data
+pd.set_option('display.max_columns', None)
 
-# Replace 'ERROR' with NaN and handle missing values
-df['Total Spent'] = pd.to_numeric(df['Total Spent'], errors='coerce')
 
-# Replace 'UNKNOWN' and empty strings with 'Unknown'
-df['Item'] = df['Item'].replace('UNKNOWN', 'Unknown').fillna('Unknown')
-df['Location'] = df['Location'].replace('UNKNOWN', 'Unknown').fillna('Unknown')
-df['Payment Method'] = df['Payment Method'].replace('ERROR', 'Unknown').fillna('Unknown')
+df = pd.read_csv('water_quality/water_potability.csv')
 
-# Convert the 'Transaction Date' column to datetime format
-df['Transaction Date'] = pd.to_datetime(df['Transaction Date'], errors='coerce')
-
-# Display data before and after cleaning
+# Выведем первые строки для анализа
 print("Before data cleaning:")
-print(df.head(10))  # Show only the first 10 rows for example
+print(df.head(10))
 
-# Save the cleaned data to a new CSV file
-df.to_csv('cleaned_transactions.csv', index=False)
+
+# Заменим пустые значения (NaN) на средние значения для числовых столбцов
+df['Hardness'] = df['Hardness'].fillna(df['Hardness'].mean())
+df['Solids'] = df['Solids'].fillna(df['Solids'].mean())
+df['Chloramines'] = df['Chloramines'].fillna(df['Chloramines'].mean())
+df['Sulfate'] = df['Sulfate'].fillna(df['Sulfate'].mean())
+df['Conductivity'] = df['Conductivity'].fillna(df['Conductivity'].mean())
+df['Organic_carbon'] = df['Organic_carbon'].fillna(df['Organic_carbon'].mean())
+df['Trihalomethanes'] = df['Trihalomethanes'].fillna(
+    df['Trihalomethanes'].mean())
+df['Turbidity'] = df['Turbidity'].fillna(df['Turbidity'].mean())
+
+df['Potability'] = df['Potability'].fillna(0)
+
+# Преобразуем столбец 'Potability' в целочисленный тип
+df['Potability'] = df['Potability'].astype(int)
+
+# Сохраним очищенные данные в новый CSV файл
+df.to_csv('cleaned_water_quality.csv', index=False)
 
 print("\nAfter data cleaning:")
 print(df.head(10))
